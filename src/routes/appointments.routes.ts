b1705918c0618e60b59e15e9843793f37aaafbc9
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2020-09-17 20:26:13 -0300

    Refatorando o arquivo de rotas para um model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already bocked!' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
