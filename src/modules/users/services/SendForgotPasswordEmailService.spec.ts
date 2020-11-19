ca29d665bde265e5ec4c9d88445c73d1fd86d0b5
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2020-11-19 11:43:16 -0300

    aplication DDD

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,32 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+import AppError from '@shared/errors/AppError';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@gmail.com',
+      password: '1234',
+    });
+
+    await sendForgotPasswordEmailService.execute({
+      email: 'gabriel@gmail.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,32 @@
+import { injectable, inject } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+// import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
