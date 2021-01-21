935bd65bce902ee16daeb539d985860e3b49b081
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-01-21 12:00:50 -0300

    add readFiletemplateMail

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
-import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'mail content';
   }
 }
 
