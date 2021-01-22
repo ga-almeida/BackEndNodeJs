843d4077c2ab29bb31d3f79c1767d4b367bf2b5b
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-01-22 16:49:52 -0300

    add updated profile tests

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,114 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+
+import AppError from '@shared/errors/AppError';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfileService: UpdateProfileService;
+
+describe('UpdateProfileService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfileService = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able update the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@profile.com.br',
+      password: '1234',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      email: 'gabriel@profile.com',
+      name: 'Gabriel Almeida',
+    });
+
+    expect(updatedUser.name).toBe('Gabriel Almeida');
+    expect(updatedUser.email).toBe('gabriel@profile.com');
+  });
+
+  it('should not be able to change to another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@profile.com.br',
+      password: '1234',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Gabriel-test',
+      email: 'gabriel-test@profile.com.br',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        email: 'gabriel@profile.com.br',
+        name: 'Gabriel Almeida',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@profile.com.br',
+      password: '1234',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      email: 'gabriel@profile.com.br',
+      name: 'Gabriel Almeida',
+      old_password: '1234',
+      new_password: '4321',
+    });
+
+    expect(updatedUser.password).toBe('4321');
+  });
+
+  it('should not be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@profile.com.br',
+      password: '1234',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        email: 'gabriel@profile.com.br',
+        name: 'Gabriel Almeida',
+        new_password: '4321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update the password without with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Gabriel',
+      email: 'gabriel@profile.com.br',
+      password: '1234',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        email: 'gabriel@profile.com.br',
+        name: 'Gabriel Almeida',
+        new_password: '4321',
+        old_password: 'wrong-old-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,74 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  new_password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    email,
+    name,
+    user_id,
+    new_password,
+    old_password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError('E-mail already in use.');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (new_password && !old_password) {
+      throw new AppError(
+        'The old password need to inform to set a new password',
+      );
+    }
+
+    if (new_password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError('The old password must be correct');
+      }
+
+      user.password = await this.hashProvider.generateHash(new_password);
+    }
+
+    const updatedUSer = await this.usersRepository.save(user);
+
+    return updatedUSer;
+  }
+}
+
+export default UpdateProfileService;
