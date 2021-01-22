1a7e14da8ed5172375c55f54f418ef60c183a7ee
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-01-22 18:00:31 -0300

    updated profileService

--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -20,7 +20,7 @@ class UpdateProfileService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('MailProvider')
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
